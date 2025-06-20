c71d851473a668879ee276fe475b8f14f3a5bc2d
Ahkla <ahkla185@hotmail.com>
2025-06-20 13:18:45 -0500

    se agregaron los diferentes metodos que se pueden usar en express con el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import server from "./server";
-
-server.get("/", (req,res) => {
-    res.send("Hello World")
+server.listen(4000,() => {
+    console.log("Server is running on port 4000");
 })
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import express from "express";
 const server = express();
 
-server.listen(4000,() => {
-    console.log("Server is running on port 4000");
-})
+
+
+server.get("/",(req,res) => {
+    res.send("GET PETITION");
+});
+
+server.post("/",(req,res) => {
+    res.send("PETITION POST")
+});
+server.put("/",(req,res) => {
+    res.send("PETITION PUT")
+});
+server.delete("/",(req,res) => {
+    res.send("PETITION DELETE")
+});
+server.patch("/",(req,res) => {
+    res.send("PETITION PATCH")
+});
+
 
 export default server;

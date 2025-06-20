b357ca4269380cad2d5509b3336f1e8dec3b67c5
Ahkla <ahkla185@hotmail.com>
2025-06-20 18:58:47 -0500

    se realizo la conexion a la base de datos usando sqlize

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import productRouter from "./router"
+import db from './config/db'
 const server = express();
 
 server.use(express.json())
 
 server.use('/api/products',productRouter)
 
-
+async function connectionDB(){
+    try{
+        await db.authenticate()
+        db.sync() //update database
+        console.log("Conexion exitosa a la base de datos ");
+    }catch{
+        console.log("Hubo un error de conexion en la base de datos");
+        
+    }
+}
+connectionDB()
 export default server;
